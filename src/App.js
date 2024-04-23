0b277867707edd9203824c83374cee633fb6eaeb
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-04-23 13:39:03 +0500

    about page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import About from "./pages/about/About";
 import Navbar from "./components/navbar/Navbar";
-// import Hero from "./components/hero/Hero";
+import Hero from "./components/hero/Hero";
 import Careers from "./pages/careers/Careers";
 import Services from "./pages/services/Services";
 import Blogpage from "./pages/blogpage/Blogpage";
 import Contact from "./pages/contact/Contact";
+import Blog from "./components/blog/Blog";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <Hero />
+      <Blog />
+      <Footer />
       <Routes>
         <Route path="http://localhost:3000/about" element={<About />} />
       </Routes>
